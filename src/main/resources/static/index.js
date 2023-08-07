eefb42494ebca6474e2555a7b7c87ccd61e40303
Kósa Balázs <emberbencsface@gmail.com>
2023-08-07 14:31:52 +0200

    deleted dead code.

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,12 +1,8 @@
-
-
 const uploadForm = document.getElementById('myformfileinput');
 const form = document.getElementById('uploadForm')
 const uploadURL = form.getAttribute("action");
 const completedCount = 0; // added for loadend scenario
-
 const token = document.querySelector("meta[name='_csrf']").getAttribute('content')
-
 //$('#_csrf').attr('content');
 const header = document.querySelector("meta[name='_csrf_header']").getAttribute('content')
 //$('#_csrf_header').attr('content');
@@ -17,7 +13,6 @@ const header = document.querySelector("meta[name='_csrf_header']").getAttribute(
         const progress_value = evt.detail.loaded/evt.detail.total * 100;
         if (isFinite(progress_value))
             htmx.find('#progress').setAttribute('value', evt.detail.loaded/evt.detail.total * 100);
-
         console.log("SETTING PROGRESS VALUE: " + evt.detail.loaded/evt.detail.total * 100)
   });
 
@@ -25,9 +20,7 @@ const header = document.querySelector("meta[name='_csrf_header']").getAttribute(
 
 
     htmx.on('#uploadForm', 'htmx:afterRequest', function(evt) {
-
         htmx.find('#progress').setAttribute('value', 0);
-
         console.log("Request finished");
     });
 
@@ -39,13 +32,8 @@ const header = document.querySelector("meta[name='_csrf_header']").getAttribute(
     });
 
 
-
-
-
-
 function uploadFiles() {
     for (let i = 0; i < uploadForm.files.length; i++) {
-
         htmx.ajax('POST', uploadURL, {
         target:'#main-file-table-tbody',
         swap:'beforeend',
@@ -53,28 +41,5 @@ function uploadFiles() {
         headers:{'X-CSRF-TOKEN': token},
         source: form // hacky way of solving this, https://github.com/bigskysoftware/htmx/issues/1560
         });
-        console.log(`file[${i}]`)
-
-
-//        const file = uploadForm.files[i];
-//        formData.append('file', file);
-//        const xmlhttp = new XMLHttpRequest();
-
-//        (elId => {
-//            xmlhttp.upload.addEventListener('progress', e => {
-//                document.getElementById('image_' + elId + '_progress').value = Math.ceil(e.loaded / e.total) * 100;
-//            }, false);
-//        })(i); // to unbind i.
-
-//        xmlhttp.addEventListener('loadend', () => {
-//            completedCount++;
-//            if (completedCount == length) {
-//                // here you should hide your gif animation
-//            }
-//        }, false);
-        // ---
-//        xmlhttp.open('POST', '/pictures/uploadImage');
-//        xmlhttp.send(formData);
     }
-
 }
