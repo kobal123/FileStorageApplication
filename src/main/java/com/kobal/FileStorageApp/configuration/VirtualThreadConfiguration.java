6f9ab8a1f427524f3987ff9cecb64f02063bc7dc
Kósa Balázs <emberbencsface@gmail.com>
2023-08-18 00:36:50 +0200

    Changed thread model to Project Loom virtual threads.

---
+++ src/main/java/com/kobal/FileStorageApp/configuration/VirtualThreadConfiguration.java
@@ -0,0 +1,26 @@
+package com.kobal.FileStorageApp.configuration;
+
+
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.support.TaskExecutorAdapter;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Executors;
+
+@Component
+public class VirtualThreadConfiguration {
+    @Bean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
+    public AsyncTaskExecutor asyncTaskExecutor() {
+        return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
+    }
+
+    @Bean
+    public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+        return protocolHandler -> {
+            protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+        };
+    }
+}
