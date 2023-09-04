d769d77639d4f2c8fac6c1d2df5305b050ddd838
Kósa Balázs <emberbencsface@gmail.com>
2023-09-05 00:11:51 +0200

    Added getters/setters.

--- src/main/java/com/kobal/FileStorageApp/user/AppUser.java
+++ src/main/java/com/kobal/FileStorageApp/user/AppUser.java
@@ -1,10 +1,6 @@
 package com.kobal.FileStorageApp.user;
 
-import com.kobal.FileStorageApp.FileMetaData;
 import jakarta.persistence.*;
-
-import java.io.File;
-import java.util.List;
 
 @Entity
 @Table(name = "AppUser")
@@ -14,7 +10,39 @@ public class AppUser {
     @Id
     private Long id;
 
-    private String username;
+    private String name;
     private String password;
 
+
+    public AppUser(Long id, String username, String password) {
+        this.id = id;
+        this.name = username;
+        this.password = password;
+    }
+
+    public AppUser() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
