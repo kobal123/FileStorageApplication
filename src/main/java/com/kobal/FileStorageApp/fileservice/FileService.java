7aa99c8f7820beba3b0306a9655ae615797bc09c
Kósa Balázs <emberbencsface@gmail.com>
2023-09-05 00:16:06 +0200

    Changed FileService interface to use FileMetaDataDTO, FilePath classes

--- src/main/java/com/kobal/FileStorageApp/fileservice/FileService.java
+++ src/main/java/com/kobal/FileStorageApp/fileservice/FileService.java
@@ -1,7 +1,12 @@
 package com.kobal.FileStorageApp.fileservice;
 
+import com.kobal.FileStorageApp.FileMetaData;
+import com.kobal.FileStorageApp.FileMetaDataDTO;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.File;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Path;
 import java.security.Principal;
 import java.util.List;
@@ -11,15 +16,17 @@ public interface FileService {
 
 
 
-    Optional<File> getFile(Principal principal, Path path);
-    void uploadFile(Principal principal, Path path, InputStream file);
+    FileMetaDataDTO uploadFile(Principal principal, FilePath path, MultipartFile file);
 
-    void createDirectory(Principal principal, Path directoryPath);
-    List<File> getFilesInDirectory(Principal principal, Path path);
+    InputStream download(Principal principal, FilePath path);
+    void createDirectory(Principal principal, FilePath directoryPath);
+    List<FileMetaDataDTO> getFilesInDirectory(Principal principal, FilePath path);
 
-    List<String> deleteFilesInDirectory(Principal principal, Path directory, List<String> files);
+    List<String> deleteFilesInDirectory(Principal principal, FilePath directory, List<String> files);
 
-    List<String> moveFilesToDirectory(Principal principal, Path from, Path to, List<String> fileNames);
+    List<String> moveFilesToDirectory(Principal principal, FilePath from, FilePath to, List<String> fileNames);
 
-    List<String> copyFilesToDirectory(Principal principal, Path from, Path to, List<String> fileNames);
+    List<String> copyFilesToDirectory(Principal principal, FilePath from, FilePath to, List<String> fileNames);
+
+    Optional<FileMetaDataDTO> getFileMetaDataByUserNameAndFilePath(String name, FilePath filePath);
 }
