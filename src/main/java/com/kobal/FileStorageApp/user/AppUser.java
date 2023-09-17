0314fc4ce8cab6ffca7f013caefeb7120f9bd1d3
Kósa Balázs <emberbencsface@gmail.com>
2023-09-17 14:36:25 +0200

    Refactored some code. refactored user entity. Added userdetailsservice.

--- src/main/java/com/kobal/FileStorageApp/user/AppUser.java
+++ src/main/java/com/kobal/FileStorageApp/user/AppUser.java
@@ -1,24 +1,56 @@
 package com.kobal.FileStorageApp.user;
 
+import com.kobal.FileStorageApp.user.role.Role;
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "AppUser")
-public class AppUser {
+public class AppUser implements UserDetails {
 
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Id
     private Long id;
 
+    @Column(unique = true, nullable = false)
     private String name;
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
     private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles = new HashSet<>();
 
-
-    public AppUser(Long id, String username, String password) {
-        this.id = id;
-        this.name = username;
+    public AppUser(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
         this.password = password;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Role> getRoles() {
+        return new HashSet<>(roles);
+    }
+
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+
+
 
     public AppUser() {}
 
@@ -38,8 +70,38 @@ public class AppUser {
         this.name = name;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>(roles);
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return String.valueOf(id);
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public void setPassword(String password) {
