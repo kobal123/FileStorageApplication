f4c26d7a4c41811d193e4868277ce81969747064
Kósa Balázs <emberbencsface@gmail.com>
2023-09-29 15:37:20 +0200

    Deleted unnecessary custom queries.

--- src/main/java/com/kobal/FileStorageApp/storage/FileMetaDataRepository.java
+++ src/main/java/com/kobal/FileStorageApp/storage/FileMetaDataRepository.java
@@ -13,7 +13,6 @@ import java.util.Optional;
 
 public interface FileMetaDataRepository extends JpaRepository<FileMetaData, Long> {
 
-    List<FileMetaData> getChildrenByUserIdAndParentPath(Long userId, String path);
 
     @Query("SELECT f FROM FileMetaData f WHERE f.path = :file_path AND f.user.id = :userId")
     List<FileMetaData> getSubFoldersByUserIdAndPath(@Param("userId") Long userId,
@@ -21,15 +20,23 @@ public interface FileMetaDataRepository extends JpaRepository<FileMetaData, Long
 
     Optional<FileMetaData> getFileMetaDataByUserIdAndPath(Long userId, String path);
 
-    Optional<FileMetaData> getFileMetaDataByUserIdAndName(Long userId, String name);
 
-    @Query("SELECT f FROM FileMetaData f WHERE CONCAT(f.path, '/', f.name) = :absolutePath AND f.user.id = :userId")
+    @Query("""
+            SELECT f FROM FileMetaData f
+             WHERE CONCAT_WS('/',f.path, f.name) = CONCAT('/', :absolutePath)
+            AND f.user.id = :userId
+            """)
     Optional<FileMetaData> findByUserIdAndAbsolutePath(@Param("userId") Long userId,
                                                        @Param("absolutePath") String absolutePath);
 
-    @Query("SELECT f FROM FileMetaData f WHERE CONCAT(f.path, '/', f.name) IN :absolutePaths AND f.user.id = :userId")
-    List<FileMetaData> findByUserIdAndAbsolutePath(@Param("userId") Long userId,
-                                                       @Param("absolutePath") List<String> absolutePaths);
+    @Query("""
+        SELECT f FROM FileMetaData f
+        WHERE f.path = :file_path and f.name IN :file_names AND f.user.id = :userId
+    """)
+    List<FileMetaData> findByUserIdAndPathAndNames(@Param("userId") Long userId,
+                                                       @Param("file_path")String filePath,
+                                                       @Param("file_names") List<String> fileNames);
+
 
     @Modifying
     @Query("UPDATE FileMetaData f SET f.path = :newFilePath WHERE f.user.id = :userId AND f.id IN :fileIds")
@@ -37,11 +44,7 @@ public interface FileMetaDataRepository extends JpaRepository<FileMetaData, Long
                                       @Param("fileIds") List<Long> fileIds,
                                       @Param("newFilePath") String path);
 
-    @Query("DELETE FROM FileMetaData f WHERE CONCAT(f.path, '/', f.name) IN :absolutePaths AND f.user.id = :userId")
-    List<FileMetaData> deleteByAbsolutePath(@Param("userId") Long userId,
-                                            @Param("absolutePaths")List<String> absolutePaths);
-
-    @Query("SELECT f FROM FileMetaData f WHERE CONCAT(f.path, '/', f.name) IN :absolutePaths AND f.user.id = :userID")
+    @Query("SELECT f FROM FileMetaData f WHERE CONCAT_WS('/', f.path, f.name) IN :absolutePaths AND f.user.id = :userID")
     List<FileMetaData> getFilesByUserIdAndPathStartingWith(@Param("userID") Long id,
                                                            @Param("absolutePaths")List<String> absolutePaths);
 
