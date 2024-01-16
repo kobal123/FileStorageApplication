b60868d56c1a9e76fbc93abccf5a32894d8ee08e
Kósa Balázs <emberbencsface@gmail.com>
2024-01-16 18:53:51 +0100

    Refactored User related repositories. Added WebSecurityFilterChain.

--- src/main/java/com/kobal/FileStorageApp/user/UserRepository.java
+++ src/main/java/com/kobal/FileStorageApp/user/UserRepository.java
@@ -1,5 +1,6 @@
 package com.kobal.FileStorageApp.user;
 
+import com.kobal.FileStorageApp.user.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<AppUser, Long> {
 
     Optional<AppUser> getUserByName(String name);
+    Optional<AppUser> getUserByEmail(String email);
+
 }
--- src/main/java/com/kobal/FileStorageApp/user/storage/UserStorageInfo.java
+++ src/main/java/com/kobal/FileStorageApp/user/storage/UserStorageInfo.java
@@ -1,6 +1,6 @@
 package com.kobal.FileStorageApp.user.storage;
 
-import com.kobal.FileStorageApp.user.AppUser;
+import com.kobal.FileStorageApp.user.model.AppUser;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
