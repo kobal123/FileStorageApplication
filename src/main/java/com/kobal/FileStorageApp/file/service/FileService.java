41c3f2667c475ceb222b7a00dc9f3c2c2cf65eb7
Kósa Balázs <emberbencsface@gmail.com>
2024-03-04 19:15:53 +0100

    Refactored FileService to use user id instead of Principal.

--- src/main/java/com/kobal/FileStorageApp/file/service/FileService.java
+++ src/main/java/com/kobal/FileStorageApp/file/service/FileService.java
@@ -4,27 +4,26 @@ import com.kobal.FileStorageApp.file.model.filemetadata.FileMetaDataDTO;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
-import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 
 public interface FileService {
 
+    Optional<FileMetaDataDTO> uploadFile(Long userId, FilePath path, MultipartFile file);
 
+    InputStream download(Long userId, FilePath path);
+    boolean createDirectory(Long userId, FilePath directoryPath);
+    List<FileMetaDataDTO> getFilesInDirectory(Long userId, FilePath path);
 
-    Optional<FileMetaDataDTO> uploadFile(Principal principal, FilePath path, MultipartFile file);
+    List<FileMetaDataDTO> deleteFilesInDirectory(Long userId, FilePath directory, List<String> files);
 
-    InputStream download(Principal principal, FilePath path);
-    boolean createDirectory(Principal principal, FilePath directoryPath);
-    List<FileMetaDataDTO> getFilesInDirectory(Principal principal, FilePath path);
+    List<FileMetaDataDTO> moveFilesToDirectory(Long userId, FilePath from, FilePath to, List<String> fileNames);
 
-    List<FileMetaDataDTO> deleteFilesInDirectory(Principal principal, FilePath directory, List<String> files);
+    void rename(Long userId, FilePath pathToFile, String newName);
 
-    List<FileMetaDataDTO> moveFilesToDirectory(Principal principal, FilePath from, FilePath to, List<String> fileNames);
+    List<FileMetaDataDTO> copyFilesToDirectory(Long userId, FilePath from, FilePath to, List<String> fileNames);
 
-    List<FileMetaDataDTO> copyFilesToDirectory(Principal principal, FilePath from, FilePath to, List<String> fileNames);
+    Optional<FileMetaDataDTO> getFileMetaDataByPath(Long userId, FilePath filePath);
 
-    Optional<FileMetaDataDTO> getFileMetaDataByPath(Principal principal, FilePath filePath);
-
-    List<FileMetaDataDTO> findFilesWithName(Principal principal, String filename);
+    List<FileMetaDataDTO> searchFiles(Long userId, String filename, boolean directoryOnly);
 }
